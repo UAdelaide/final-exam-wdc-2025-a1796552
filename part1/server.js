d6b5a9da50b22be4103f90bc8ad2b33c33bd4410
Sunggil Kang <liggnusgank@gmail.com>
2025-06-20 00:12:16 +0000

    [automatic commit] 2025-06-20 00:12:16 UTC 144.6.29.237

--- part1/server.js
+++ part1/server.js
@@ -4,15 +4,13 @@ const fs = require('fs');
 const app = express();
 const PORT = 8080;
 
-// Create MySQL pool
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',         // Change this if your MySQL username is different
-  password: '',         // Add your MySQL password if you have one
+  user: 'root',
+  password: '',
   multipleStatements: true
 });
 
-// Run dogwalks.sql on startup
 const schema = fs.readFileSync('./part1/dogwalks.sql', 'utf8');
 pool.query(schema, (err) => {
   if (err) {
@@ -22,18 +20,15 @@ pool.query(schema, (err) => {
   }
 });
 
-// Attach pool to all requests
 app.use((req, res, next) => {
   req.pool = pool;
   next();
 });
 
-// Example test route
 app.get('/', (req, res) => {
   res.send('API is running.');
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
